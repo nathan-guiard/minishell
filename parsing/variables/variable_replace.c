cd7d0b18cf9fa7b13572e229ca575f4b0d00f576
Nathan G <nguiard@student.42.fr>
2022-04-06 16:42:17 +0200

    Norm pour les 25 lignes, de rien pour ca

--- parsing/variables/variable_replace.c
+++ parsing/variables/variable_replace.c
@@ -6,44 +6,26 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 12:16:54 by nguiard           #+#    #+#             */
-/*   Updated: 2022/03/28 13:30:34 by nguiard          ###   ########.fr       */
+/*   Updated: 2022/04/06 16:41:22 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "variables.h"
 
+char	*change_the_variable(char *line, int *i);
+
 /*	Remplace les variables par leur valeur dans l'environnement
 	Retour la ligne changee ou alors retourne S_ERR/S_MERR en cas d'erreur	*/
 char	*replace_variables(char *line)
 {
 	int	i;
-	int	ret;
 
-	ret = 0;
 	i = 0;
 	while (line[i] != '\0')
 	{
 		if (line[i] == VALID_DOLLAR)
-		{
-			ret = is_a_valid_env(line, i);
-			if (ret == M_ERR)
-				return (S_MERR);
-			else if (ret == TRUE)
-			{
-				line = replace_variable_by_content(line, i);
-				if (ft_strcmp(S_ERR, line) == 0)
-					return (S_ERR);
-				i = 0;
-			}
-			else if (ret == FALSE)
-			{
-				line = replace_variable_by_nothing(line, i);
-				if (ft_strcmp(S_ERR, line) == 0)
-					return (S_ERR);
-				i = 0;
-			}
-		}
+			line = change_the_variable(line, &i);
 		if ((size_t)i < ft_strlen(line))
 			i++;
 	}
@@ -119,4 +101,28 @@ char	*replace_variable_by_nothing(char *line, int start_var)
 	free(part_two);
 	free(line);
 	return (res);
+}
+
+char	*change_the_variable(char *line, int *i)
+{
+	int	ret;
+
+	ret = is_a_valid_env(line, *i);
+	if (ret == M_ERR)
+		return (S_MERR);
+	else if (ret == TRUE)
+	{
+		line = replace_variable_by_content(line, *i);
+		if (ft_strcmp(S_ERR, line) == 0)
+			return (S_ERR);
+		*i = 0;
+	}
+	else if (ret == FALSE)
+	{
+		line = replace_variable_by_nothing(line, *i);
+		if (ft_strcmp(S_ERR, line) == 0)
+			return (S_ERR);
+		*i = 0;
+	}
+	return (line);
 }
