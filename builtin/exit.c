f957d9171906edfa9a66f3c33c2ccc20800555f8
nguiard <nguiard@student.42.fr>
2022-05-04 16:55:05 +0200

    norme

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 19:32:04 by nguiard           #+#    #+#             */
-/*   Updated: 2022/04/05 11:34:22 by nguiard          ###   ########.fr       */
+/*   Updated: 2022/05/04 16:54:04 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void	ft_exit(int exit_value, char **args)
 		exit_with_args(args);
 	else
 	{
-		//Free tout le bullshit
 		ft_lstclear(&g_env, free);
 		rl_clear_history();
 		exit(exit_value);
